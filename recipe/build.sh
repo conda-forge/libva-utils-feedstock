2bb5c8de73ffb3731a0b313b0d7b2b461a7258e7
Mark Harfouche <mark.harfouche@gmail.com>
2019-04-05 20:42:37 -0400

    remove -std=c++99 from CPPFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 # Maybe these cflags can be removed with the new compilers
 set -ev
 CFLAGS="${CFLAGS} -std=c99 -lrt"
-CXXFLAGS="${CXXFLAGS} -std=c99 -lrt"
+CXXFLAGS="${CXXFLAGS} -lrt"
 
 ./autogen.sh --prefix=$PREFIX
 make -j$(nproc) install

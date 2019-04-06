811baa1b62ee2da5243d19a7ce2d158d7847794a
Mark Harfouche <mark.harfouche@gmail.com>
2019-04-05 20:34:59 -0400

    Add -lrt

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,8 @@
 # https://01.org/linuxgraphics/documentation/build-guide-0
 # Maybe these cflags can be removed with the new compilers
 set -ev
-CFLAGS="${CFLAGS} -std=c99"
-CXXFLAGS="${CXXFLAGS} -std=c99"
+CFLAGS="${CFLAGS} -std=c99 -lrt"
+CXXFLAGS="${CXXFLAGS} -std=c99 -lrt"
 
 ./autogen.sh --prefix=$PREFIX
 make -j$(nproc) install

d6d2d8351bd84d7d5b7841d4932ec1ea00cdddad
Mark Harfouche <mark.harfouche@gmail.com>
2021-03-23 08:35:30 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,8 @@
 # https://01.org/linuxgraphics/documentation/build-guide-0
 # Maybe these cflags can be removed with the new compilers
 set -ev
-CFLAGS="${CFLAGS} -std=c99 -lrt"
-CXXFLAGS="${CXXFLAGS} -lrt"
+# CFLAGS="${CFLAGS} -std=c99 -lrt"
+# CXXFLAGS="${CXXFLAGS} -lrt"
 
 ./autogen.sh --prefix=$PREFIX --disable-x11
 make -j$(nproc) install

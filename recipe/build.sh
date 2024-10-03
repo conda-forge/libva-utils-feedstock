a7ca0e847e50b1ba6a6345d9d3f3b2e1d6f52062
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-10-03 23:50:32 +0900

    Modernize libva-utils workflow

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,17 @@
-# https://01.org/linuxgraphics/documentation/build-guide-0
-# Maybe these cflags can be removed with the new compilers
-set -ev
-CFLAGS="${CFLAGS} -lrt"
-CXXFLAGS="${CXXFLAGS} -lrt"
+#!/bin/bash
+set -e -x
 
-./autogen.sh --prefix=$PREFIX --disable-x11
-make -j$(nproc) install
-set +ev
+mkdir -pm755 build
+pushd build
+
+export XDG_DATA_DIRS="${XDG_DATA_DIRS:+${XDG_DATA_DIRS}:}:${PREFIX}/share:${BUILD_PREFIX}/share"
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+${PKG_CONFIG_PATH}:}${PREFIX}/lib/pkgconfig:${BUILD_PREFIX}/lib/pkgconfig"
+
+export PKG_CONFIG="$(which pkg-config)"
+
+meson setup ${MESON_ARGS} ..
+
+ninja
+ninja install
+
+popd
